2ae8687dcbd850eaf78f889d90ab4162c9b90b5a
Umherirrender <umherirrender_de.wp@web.de>
2018-03-17 23:13:35 +0100

    Remove DO_MAINTENANCE compat code with pre1.17 version
    Change-Id: I62df5f63574a9b56a36751d3feecbf39bad5a030

--- maintenance/usageTrackerUpdate.php
+++ maintenance/usageTrackerUpdate.php
@@ -33,8 +33,4 @@ class UsageTrackerUpdate extends BSMaintenance {
 }
 
 $maintClass = 'UsageTrackerUpdate';
-if (defined('RUN_MAINTENANCE_IF_MAIN')) {
-	require_once( RUN_MAINTENANCE_IF_MAIN );
-} else {
-	require_once( DO_MAINTENANCE ); # Make this work on versions before 1.17
-}
+require_once RUN_MAINTENANCE_IF_MAIN;
