f6205a02fe51917b2b15a4dd866dfe43918bb6dc
root <support@hallowelt.com>
2022-03-11 12:53:48 +0100

    Add maintenance script for instance statistics
    ERM28550
    
    [4.1.x]
    
    Change-Id: I8012866bb1bf57af61c7f4642b21593d182dd017

---
+++ maintenance/showInstanceStats.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Show the cached statistics.
+ * Give out the same output as [[Special:Statistics]]
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ * @author Antoine Musso <hashar at free dot fr>
+ * Based on initSiteStats.php by:
+ * @author Brion Vibber
+ * @author Rob Church <robchur@gmail.com>
+ *
+ * @license GPL-2.0-or-later
+ */
+
+use MediaWiki\MediaWikiServices;
+
+$IP = realpath( dirname( dirname( __DIR__ ) ) );
+
+require_once $IP . '/BlueSpiceFoundation/maintenance/BSMaintenance.php';
+
+/**
+ * Maintenance script to show the cached statistics.
+ *
+ * @ingroup Maintenance
+ */
+class ShowInstanceStats extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Show the cached statistics' );
+	}
+
+	public function execute() {
+		$fields = [
+			'ss_total_edits' => 'Total edits',
+			'ss_good_articles' => 'Number of articles',
+			'ss_total_pages' => 'Total pages',
+			'ss_users' => 'Number of users',
+			'ss_active_users' => 'Active users',
+			'ss_images' => 'Number of images',
+		];
+
+		// Get cached stats from a replica DB
+		$dbr = $this->getDB( DB_REPLICA );
+		$stats = $dbr->selectRow( 'site_stats', '*', '', __METHOD__ );
+
+		// Get maximum size for each column
+		$maxlengthvalue = $maxlengthdesc = 0;
+		foreach ( $fields as $field => $desc ) {
+			$maxlengthvalue = max( $maxlengthvalue, strlen( $stats->$field ) );
+			$maxlengthdesc = max( $maxlengthdesc, strlen( $desc ) );
+		}
+		$sitestats = [];
+		foreach ( $fields as $field => $desc ) {
+			array_push( $sitestats, [ $desc => $stats->$field ]
+			);
+		}
+		$em = MediaWikiServices::getInstance()->getService( 'BSExtensionFactory' );
+		$usagetrackerdata = $em->getExtension( 'BlueSpiceUsageTracker' )->getUsageDataFromDB();
+		$usagetracker = [];
+		foreach ( $usagetrackerdata as $data ) {
+			array_push( $usagetracker, [ $data->identifier => $data->count ] );
+		}
+		$instanceStats = [
+			"sitestats" => call_user_func_array( 'array_merge', $sitestats ),
+			"usagetracker" => call_user_func_array( 'array_merge', $usagetracker )
+		];
+		$this->output( json_encode( $instanceStats, JSON_PRETTY_PRINT ) );
+	}
+}
+
+$maintClass = ShowInstanceStats::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
